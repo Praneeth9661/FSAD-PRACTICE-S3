9cb7c98fe768e578288275b6cf3550b2e471f7fe
Praneeth9661 <srirampraneeth904@gmail.com>
2026-01-28 10:13:32 +0530

    "28-01-2026"

---
+++ SpringBootDemoApp/src/main/java/com/klu/controller/Demo.java
@@ -0,0 +1,16 @@
+package com.klu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo {
+	
+	@GetMapping("/hello")
+	
+	public String sayHello() {
+		return "Spring Root Demo Application";
+	}
+	
+
+}
